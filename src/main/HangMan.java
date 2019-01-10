be53b4bf22e1ad0d4a6c2c9f35bc87748c927df5
Aamod Krishna Tiwari <aamod.tiwari_ccv17@gla.ac.in>
2019-01-10 15:04:27 +0530

    changed

---
+++ src/main/HangMan.java
@@ -0,0 +1,28 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HangMan
+{
+    public static void main(String[] args) {
+        System.out.println("enter the secret word");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        StringBuffer word;
+
+        try {
+            word = new StringBuffer(br.readLine());
+            System.out.println("your word is"+word);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+}
